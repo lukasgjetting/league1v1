73cebe4adf8b65a7aede8e035e424769829ed89b
Lukas Gjetting <lukasgjetting@gmail.com>
2022-01-24 21:29:09 +0100

    Set up Next project

---
+++ pages/auth.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export const getServerSideProps = async () => {
+
+};
+
+const AuthScreen = () => {
+    const [username, setUsername] = useState('');
+
+    return null;
+};
+
+export default AuthScreen;
