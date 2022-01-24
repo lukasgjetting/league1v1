f31b9610c821bcfa97944cb5b2df33a7e8e7754e
Lukas Gjetting <lukasgjetting@gmail.com>
2022-01-24 21:31:52 +0100

    Start implementing auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 export const getServerSideProps = async () => {
-
+    return {
+        props: {},
+    };
 };
 
 const AuthScreen = () => {
     const [username, setUsername] = useState('');
 
-    return null;
+    return (
+        <div>Please enter your League of Legends summoner name</div>
+    );
 };
 
 export default AuthScreen;
